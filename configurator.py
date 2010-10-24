f654bb92ee5a07cfcee9b751e3c8c19025a7d5c2
Johannas Heller <johann@phyfus.com>
2010-10-24 03:38:18 -0700

    Added a basic configurator class.

---
+++ configurator.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import optparse
+
+import jinja2
+from jinja2 import meta
+
+
+class Configurator(object):
+    usage = 'usage: %prog template [CONTEXT_VARIABLES]'
+
+    EXIT_VALUES = {
+        'NO TEMPLATE PROVIDED': 1,
+        'TEMPLATE NOT FOUND': 2,
+        'CONTEXT VARIABLE NOT PROVIDED': 3,
+    }
+
+    def __init__(self, template_dir):
+        self.loader = jinja2.FileSystemLoader(template_dir)
+        self.env = jinja2.Environment(loader=self.loader, autoescape=False)
+
+    def run(self, args):
+        template_name = self.get_template_name(args)
+        context = self.get_template_context(template_name, args)
+        output = self.render_template(template_name, context)
+        print output
+
+    def get_template_name(self, args):
+        if len(args) == 0:
+            print self.usage.replace('%prog', 'configurator.py')
+            print
+            print 'ERROR: you must specify a template'
+            sys.exit(self.EXIT_VALUES['NO TEMPLATE PROVIDED'])
+        return args[0]
+
+    def get_template_variables(self, template_name):
+        try:
+            (source, filename, uptodate) = self.loader.get_source(self.env, template_name)
+        except jinja2.TemplateNotFound as error:
+            print 'ERROR: can not find template "%s"' % template_name
+            print 'ERROR: search path was "%s"' % self.loader.searchpath
+            sys.exit(self.EXIT_VALUES['TEMPLATE NOT FOUND'])
+        ast = self.env.parse(source)
+        variables = meta.find_undeclared_variables(ast)
+        return variables
+
+    def get_options(self, variables, args):
+        parser = optparse.OptionParser(usage=self.usage)
+
+        for var in variables:
+            help_text = 'You must provide a value for "%s".' % var
+            parser.add_option('--%s' % var, dest=var, help=help_text)
+
+        (options, args) = parser.parse_args(args)
+        return options
+
+    def raise_error_on_missing_variables(self, variables, context):
+        error = 'ERROR: "%s" is used in the template.\n'
+        error += 'ERROR: please provide a value for it by using "--%s=VALUE"'
+        for var in variables:
+            if context[var] is None:
+                print error % (var, var)
+                sys.exit(self.EXIT_VALUES['CONTEXT VARIABLE NOT PROVIDED'])
+
+    def get_context_from_options(self, variables, options):
+        context = dict()
+        for var in variables:
+            context[var] = getattr(options, var)
+        return context
+
+    def get_template_context(self, template_name, args):
+        variables = self.get_template_variables(template_name)
+
+        options = self.get_options(variables, args)
+
+        context = self.get_context_from_options(variables, options)
+
+        self.raise_error_on_missing_variables(variables, context)
+
+        return context
+
+    def render_template(self, template_name, context):
+        template = self.env.get_template(template_name)
+        return template.render(**context)
+
+
+if __name__ == '__main__':
+    base = os.path.abspath(os.path.dirname(__file__))
+    template_dir = os.path.join(base, 'templates')
+    args = sys.argv[1:]
+
+    configurator = Configurator(template_dir)
+    configurator.run(args)
